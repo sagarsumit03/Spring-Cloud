12ba8ea02cc6ac6c5ac26a660b88d8549d768e65
sagar <sagarsumit03@gmail.com>
2018-07-17 10:31:41 +0530

    Added Hystrix and Sleuth

--- consume-client/src/main/java/com/sumit/spring/advance/spring_cloud/ConsumerClientApplication.java
+++ consume-client/src/main/java/com/sumit/spring/advance/spring_cloud/ConsumerClientApplication.java
@@ -3,6 +3,7 @@ package com.sumit.spring.advance.spring_cloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @EnableEurekaClient
 @RestController
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 //@RibbonClient(name = "expose-client", configuration = SayHelloConfiguration.class)
 @SpringBootApplication
 public class ConsumerClientApplication {
@@ -39,6 +43,17 @@ public class ConsumerClientApplication {
 	@RequestMapping("/consume")
 	public String hello() {
 		return restTemplate.getForObject("http://expose-client/hello", String.class);
+	}
+
+	@HystrixCommand(fallbackMethod = "fallback")
+	@RequestMapping("/hystrix")
+	public String hystrix() {
+		return restTemplate.getForObject("http://expose-client/hello", String.class);
+	}
+
+	// a fallback method to be called if failure happened
+	public String fallback(int galleryId, Throwable hystrixCommand) {
+		return "Circuit Breaker fallback function";
 	}
 
 }
--- expose-client/src/main/java/com/example/expose/demo/ExposeClientApplication.java
+++ expose-client/src/main/java/com/example/expose/demo/ExposeClientApplication.java
@@ -5,20 +5,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class ExposeClientApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExposeClientApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExposeClientApplication.class, args);
 	}
-	
-	
-	
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello From Exposed Client";
+	}
+
+	@GetMapping("/circuit-breaker")
+	public String hystrix() throws Exception {
+		throw new Exception("exception for Hystrix");
+	}
+
+	@GetMapping("/sleuth")
+	public String sleuth() {
+		LOGGER.info("Into ExposeClient Sleuth method ... ");
+		return "Text from Sleuth";
 	}
 }
